159b3e4e2d6266243044aa81db0b4e8ace18b231
reflective-mapping-9478 <ejrobe@gmail.com>
2018-11-06 00:38:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var response = []
+  for (var i = 0; i < musicians.length; i++) {
+    response[i] = musicians[i] + " plays " + instruments[i]
+  }
+  return response
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  while (i < facts.length){
+    facts[i] = facts[i] + "!!!";
+    i++;
+  }
+  return facts
+}
+
+function iLoveTheBeatles(int){
+  var response = [];
+  do {
+    response.push("I love the beatles!")
+    int--
+  } while (int > 0)
+  return response
+}
